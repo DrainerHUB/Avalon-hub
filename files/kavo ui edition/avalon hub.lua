44a6e569d92cf6fd88e4c9bf8e617cc1aa872d2c
Drainer <167999355+DrainerHUB@users.noreply.github.com>
2024-05-04 16:57:47 +0300

    upd

--- files/kavo ui edition/avalon hub.lua
+++ files/kavo ui edition/avalon hub.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 17081469302 then
     local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
-    local Window = Library.CreateLib("Avalon Hub Beta | Test Game", "Serpent")
+    local Window = Library.CreateLib("Drainer Hub", "Serpent")
      
     -- Main
     local Main = Window:NewTab("Main")
@@ -20,86 +20,6 @@ if game.PlaceId == 17081469302 then
         game.Players.LocalPlayer.Character.Humanoid.JumpHeight = 50
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
     end)
-    PlayerSection:NewToggle("Esp", "esp", function(Value)
-		getgenv().esp = Value
-		spawn(function()
-		while wait() do
-		    if not getgenv().esp then
-		          for i,v in pairs(game.Players:GetChildren()) do
-		              if v.Character and v.Character:FindFirstChild("Highlight") then
-		                  local Highlight = v.Character:FindFirstChild("Highlight")
-		                  Highlight.Enabled = false
-    		      end
-		      end 
-		      else
-		          for i,v in pairs(game.Players:GetChildren()) do
-		             if getgenv().teamcheck == true then
-		               if v.Character and v ~= game.Players.LocalPlayer and v.TeamColor ~= game.Players.LocalPlayer.TeamColor then
-    		                 if v.Character:FindFirstChild("Highlight") then
-    		                 local Highlight = v.Character:FindFirstChild("Highlight") 
-    		                 Highlight.Enabled = true
-    		                 Highlight.FillColor = getgenv().Color
-    		                 Highlight.Adornee = v.Character
-    		                 else
-    		                 local Highlight = Instance.new("Highlight",v.Character)
-    		                 Highlight.Enabled = true
-    		                 Highlight.FillColor = getgenv().Color
-    		                 Highlight.Adornee = v.Character
-    		              end       
-    		           end  
-		                if v.TeamColor == game.Players.LocalPlayer.TeamColor then
-    		              if v.Character and v.Character:FindFirstChild("Highlight") then
-    		                  local Highlight = v.Character:FindFirstChild("Highlight")
-        		              Highlight.Enabled = false
-        		          end    
-    		            end 
-    		          else
-    		              if v.Character and v ~= game.Players.LocalPlayer then
-    		                 if v.Character:FindFirstChild("Highlight") then
-    		                 local Highlight = v.Character:FindFirstChild("Highlight") 
-    		                 Highlight.Enabled = true
-    		                 Highlight.FillColor = getgenv().Color
-    		                 Highlight.Adornee = v.Character
-    		                 else
-    		                 local Highlight = Instance.new("Highlight",v.Character)
-    		                 Highlight.Enabled = true
-    		                 Highlight.FillColor = getgenv().Color
-    		                 Highlight.Adornee = v.Character
-    		              end       
-    		           end    
-		            end       
-		      end    
-		    end  
-		end    
-	end)
-    PlayerSection:NewColorPicker("Color Text", "Color Info", Color3.fromRGB(0,0,0), function(Value)
-        getgenv().Color = Value
-    end)
-    PlayerSection:NewToggle("TeamCheck", "TeamCheck", function(Value)
-        getgenv().teamcheck = Value
-        end
-    end)
-    PlayerSection:NewToggle("Rainbow Esp", "Rainbow Esp", function(Value)
-            getgenv().Rainbow = Value
-            while wait() do
-                if not getgenv().Rainbow then return end
-                getgenv().Color = Color3.new(148, 0, 211)
-                wait()
-                getgenv().Color = Color3.new(75, 0, 130)
-                wait()
-                getgenv().Color = Color3.new(0, 0, 255)
-                wait()
-                getgenv().Color = Color3.new(0, 255, 0)
-                wait()
-                getgenv().Color = Color3.new(255, 255, 0)
-                wait()
-                getgenv().Color = Color3.new(255, 127, 0)
-                wait()
-                getgenv().Color = Color3.new(255, 0 , 0)
-                wait()
-            end    
-        end    
-    end)
     -- Tools
     local Tools = Window:NewTab("Tools")
     local ToolsSection = Tools:NewSection("Tools")
@@ -112,7 +32,7 @@ if game.PlaceId == 17081469302 then
      
     elseif game.PlaceId == 16389398622 then
     local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
-    local Window = Library.CreateLib("Avalon Hub Beta | A dusty trip", "Serpent")
+    local Window = Library.CreateLib("Drainer Hub Beta | A dusty trip", "Serpent")
      
     -- Main
     local Main = Window:NewTab("Main")
@@ -130,86 +50,6 @@ if game.PlaceId == 17081469302 then
     PlayerSection:NewButton("Reset WS/JP", "Resets your jumppower", function()
         game.Players.LocalPlayer.Character.Humanoid.JumpHeight = 50
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-    end)
-    PlayerSection:NewToggle("Esp", "esp", function(Value)
-		getgenv().esp = Value
-		spawn(function()
-		while wait() do
-		    if not getgenv().esp then
-		          for i,v in pairs(game.Players:GetChildren()) do
-		              if v.Character and v.Character:FindFirstChild("Highlight") then
-		                  local Highlight = v.Character:FindFirstChild("Highlight")
-		                  Highlight.Enabled = false
-    		      end
-		      end 
-		      else
-		          for i,v in pairs(game.Players:GetChildren()) do
-		             if getgenv().teamcheck == true then
-		               if v.Character and v ~= game.Players.LocalPlayer and v.TeamColor ~= game.Players.LocalPlayer.TeamColor then
-    		                 if v.Character:FindFirstChild("Highlight") then
-    		                 local Highlight = v.Character:FindFirstChild("Highlight") 
-    		                 Highlight.Enabled = true
-    		                 Highlight.FillColor = getgenv().Color
-    		                 Highlight.Adornee = v.Character
-    		                 else
-    		                 local Highlight = Instance.new("Highlight",v.Character)
-    		                 Highlight.Enabled = true
-    		                 Highlight.FillColor = getgenv().Color
-    		                 Highlight.Adornee = v.Character
-    		              end       
-    		           end  
-		                if v.TeamColor == game.Players.LocalPlayer.TeamColor then
-    		              if v.Character and v.Character:FindFirstChild("Highlight") then
-    		                  local Highlight = v.Character:FindFirstChild("Highlight")
-        		              Highlight.Enabled = false
-        		          end    
-    		            end 
-    		          else
-    		              if v.Character and v ~= game.Players.LocalPlayer then
-    		                 if v.Character:FindFirstChild("Highlight") then
-    		                 local Highlight = v.Character:FindFirstChild("Highlight") 
-    		                 Highlight.Enabled = true
-    		                 Highlight.FillColor = getgenv().Color
-    		                 Highlight.Adornee = v.Character
-    		                 else
-    		                 local Highlight = Instance.new("Highlight",v.Character)
-    		                 Highlight.Enabled = true
-    		                 Highlight.FillColor = getgenv().Color
-    		                 Highlight.Adornee = v.Character
-    		              end       
-    		           end    
-		            end       
-		      end    
-		    end  
-		end    
-	end)
-    PlayerSection:NewColorPicker("Color Text", "Color Info", Color3.fromRGB(0,0,0), function(Value)
-        getgenv().Color = Value
-    end)
-    PlayerSection:NewToggle("TeamCheck", "TeamCheck", function(Value)
-        getgenv().teamcheck = Value
-        end
-    end)
-    PlayerSection:NewToggle("Rainbow Esp", "Rainbow Esp", function(Value)
-            getgenv().Rainbow = Value
-            while wait() do
-                if not getgenv().Rainbow then return end
-                getgenv().Color = Color3.new(148, 0, 211)
-                wait()
-                getgenv().Color = Color3.new(75, 0, 130)
-                wait()
-                getgenv().Color = Color3.new(0, 0, 255)
-                wait()
-                getgenv().Color = Color3.new(0, 255, 0)
-                wait()
-                getgenv().Color = Color3.new(255, 255, 0)
-                wait()
-                getgenv().Color = Color3.new(255, 127, 0)
-                wait()
-                getgenv().Color = Color3.new(255, 0 , 0)
-                wait()
-            end    
-        end    
     end)
     -- Tools
     local Tools = Window:NewTab("Tools")
