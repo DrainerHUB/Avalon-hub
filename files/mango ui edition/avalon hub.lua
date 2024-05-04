e48d2648f026cf05afad1f6caa4381bd52b8a69a
Drainer <167999355+DrainerHUB@users.noreply.github.com>
2024-05-04 16:37:44 +0300

    upd

--- files/mango ui edition/avalon hub.lua
+++ files/mango ui edition/avalon hub.lua
@@ -1,10 +1,10 @@
 if game.PlaceId == 17081469302 then
 local MangoLib = loadstring(game:HttpGet("https://pastebin.com/raw/N3sKKx0a"))()
 -- Main
-local win = MangoLib:Window("Avalon Hub Beta")
-local Main = win:Tab("Main")
+local menu = MangoLib:Window("Avalon Hub Beta")
+local Main = menu:Tab("Main")
 -- Player
-local Player = win:Tab("Player")
+local Player = menu:Tab("Player")
 Player:Slider("Walk Speed", 16, 500, 50, function(Value)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
 end)
